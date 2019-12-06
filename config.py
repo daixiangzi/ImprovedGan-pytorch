65a7460292ceeb286092a3fad858bef506a34054
daixiangzi <543826458@qq.com>
2019-12-06 15:21:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
-        data_dir = './data/'#train data and test data dir
+        data_dir = './data/cifar-10-batches-py'#train data and test data dir
         save_img = './save_img/' #save gen imgs dir
-        gpu_id = '0,1' #gpu id
+        gpu_id = '0' #gpu id
         train_batch_size=128
         test_batch_size=500 
         epochs= 1200 # train epochs
